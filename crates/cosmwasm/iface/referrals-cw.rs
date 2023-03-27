8d270f33683762e29c459984dff04aa367b153fb
Chris Ricketts <chris-ricketts@proton.me>
2023-03-27 10:50:22 +0100

    fix: manual deserialize for flattened execute message field

--- crates/cosmwasm/iface/referrals-cw.rs
+++ crates/cosmwasm/iface/referrals-cw.rs
@@ -1,7 +1,7 @@
 #![deny(clippy::all)]
 #![warn(clippy::pedantic)]
 
-use cosmwasm_schema::cw_serde;
+use cosmwasm_schema::{cw_serde, schemars::JsonSchema, serde::Deserialize, serde::Serialize};
 use cosmwasm_std::Uint128;
 
 #[path = "rewards-pot-cw.rs"]
@@ -13,16 +13,24 @@ pub struct InstantiateMsg {
     pub rewards_pot_code_id: u64,
 }
 
-#[cw_serde]
+#[derive(Serialize, Clone, Debug, PartialEq, JsonSchema)]
+#[serde(crate = "::cosmwasm_schema::serde")]
+#[schemars(crate = "::cosmwasm_schema::schemars")]
 pub struct WithReferralCode<Msg> {
     /// Referral code of sender
     pub referral_code: Option<u64>,
     /// Contract Execution Msg
+    // NOTE: Requires custom Deserialize impl.
+    // See: https://github.com/CosmWasm/serde-json-wasm/issues/43
     #[serde(flatten)]
     pub msg: Msg,
 }
 
-#[cw_serde]
+// NOTE: Cannot use `#[cw_serde]` due to it's use of `#[serde(deny_unknown_fields)]`.
+// Incompatible with `flatten`: https://serde.rs/container-attrs.html#deny_unknown_fields
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+#[serde(rename_all = "snake_case", crate = "::cosmwasm_schema::serde")]
+#[schemars(crate = "::cosmwasm_schema::schemars")]
 pub enum ExecuteMsg {
     /// Register as a referrer.
     /// Responds with `ReferralCodeResponse`
@@ -105,5 +113,150 @@ impl From<ExecuteMsg> for WithReferralCode<ExecuteMsg> {
             referral_code: None,
             msg,
         }
+    }
+}
+
+impl<'de, Msg> Deserialize<'de> for WithReferralCode<Msg>
+where
+    Msg: Deserialize<'de>,
+{
+    #[allow(clippy::too_many_lines)] // pedantic paperclip smh
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: cosmwasm_schema::serde::Deserializer<'de>,
+    {
+        use std::fmt;
+        use std::marker::PhantomData;
+
+        use cosmwasm_schema::serde;
+        use serde::de::{self, Deserializer, MapAccess, Visitor};
+        use serde_cw_value::Value;
+
+        #[derive(Debug)]
+        enum Field {
+            ReferralCode,
+            Msg(Value),
+        }
+
+        const FIELDS: &[&str] = &["referral_code"];
+
+        impl<'de> Deserialize<'de> for Field {
+            fn deserialize<D>(deserializer: D) -> Result<Field, D::Error>
+            where
+                D: Deserializer<'de>,
+            {
+                struct FieldVisitor;
+
+                impl<'de> Visitor<'de> for FieldVisitor {
+                    type Value = Field;
+
+                    fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+                        formatter.write_str("`referral_code`")
+                    }
+
+                    fn visit_str<E>(self, v: &str) -> Result<Self::Value, E>
+                    where
+                        E: de::Error,
+                    {
+                        match v {
+                            "referral_code" => Ok(Field::ReferralCode),
+                            _ => Ok(Field::Msg(Value::String(v.to_owned()))),
+                        }
+                    }
+
+                    fn visit_borrowed_str<E>(self, v: &'de str) -> Result<Self::Value, E>
+                    where
+                        E: de::Error,
+                    {
+                        match v {
+                            "referral_code" => Ok(Field::ReferralCode),
+                            _ => Ok(Field::Msg(Value::String(v.to_owned()))),
+                        }
+                    }
+
+                    fn visit_bytes<E>(self, v: &[u8]) -> Result<Self::Value, E>
+                    where
+                        E: de::Error,
+                    {
+                        match v {
+                            b"referral_code" => Ok(Field::ReferralCode),
+                            _ => Ok(Field::Msg(Value::Bytes(v.to_owned()))),
+                        }
+                    }
+
+                    fn visit_byte_buf<E>(self, v: Vec<u8>) -> Result<Self::Value, E>
+                    where
+                        E: de::Error,
+                    {
+                        match v.as_slice() {
+                            b"referral_code" => Ok(Field::ReferralCode),
+                            _ => Ok(Field::Msg(Value::Bytes(v))),
+                        }
+                    }
+
+                    fn visit_borrowed_bytes<E>(self, v: &'de [u8]) -> Result<Self::Value, E>
+                    where
+                        E: de::Error,
+                    {
+                        match v {
+                            b"referral_code" => Ok(Field::ReferralCode),
+                            _ => Ok(Field::Msg(Value::Bytes(v.to_owned()))),
+                        }
+                    }
+                }
+
+                deserializer.deserialize_identifier(FieldVisitor)
+            }
+        }
+
+        struct WithReferalCodeVisitor<Msg> {
+            _m: PhantomData<Msg>,
+        }
+
+        impl<'de, Msg> Visitor<'de> for WithReferalCodeVisitor<Msg>
+        where
+            Msg: Deserialize<'de>,
+        {
+            type Value = WithReferralCode<Msg>;
+
+            fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+                formatter.write_str("struct WithReferralCode")
+            }
+
+            fn visit_map<V>(self, mut map: V) -> Result<Self::Value, V::Error>
+            where
+                V: MapAccess<'de>,
+            {
+                let mut referral_code = None;
+                let mut msg = vec![];
+
+                while let Some(key) = map.next_key()? {
+                    match key {
+                        Field::ReferralCode => {
+                            if referral_code.is_some() {
+                                return Err(de::Error::duplicate_field("referral_code"));
+                            }
+                            referral_code = map.next_value()?;
+                        }
+                        Field::Msg(key) => {
+                            let value = map.next_value()?;
+                            msg.push((key, value));
+                        }
+                    }
+                }
+
+                Ok(WithReferralCode {
+                    referral_code,
+                    msg: Msg::deserialize(Value::Map(msg.into_iter().collect()))
+                        .map_err(|err| de::Error::custom(err.to_string()))?,
+                })
+            }
+        }
+
+        deserializer.deserialize_struct(
+            "WithReferralCode",
+            FIELDS,
+            WithReferalCodeVisitor { _m: PhantomData },
+        )
     }
 }
--- tests/referrals_cw.rs
+++ tests/referrals_cw.rs
@@ -22,5 +22,99 @@ pub fn with_referral_code_json_serde() {
     )
     .unwrap();
 
-    assert!(msg.referral_code.is_none())
+    assert!(matches!(
+        msg,
+        WithReferralCode {
+            referral_code: None,
+            msg: ExecuteMsg::RegisterReferrer {}
+        }
+    ));
+
+    check(
+        to_string(&WithReferralCode {
+            referral_code: Some(69),
+            msg: ExecuteMsg::RegisterReferrer {},
+        })
+        .unwrap(),
+        expect![[r#"{"referral_code":69,"register_referrer":{}}"#]],
+    );
+
+    let msg: WithReferralCode<ExecuteMsg> = from_str(
+        "{
+            \"referral_code\": 69,
+            \"register_referrer\": {}
+        }",
+    )
+    .unwrap();
+
+    assert!(matches!(
+        msg,
+        WithReferralCode {
+            referral_code: Some(69),
+            msg: ExecuteMsg::RegisterReferrer {}
+        }
+    ));
+
+    check(
+        to_string(&WithReferralCode {
+            referral_code: None,
+            msg: ExecuteMsg::ConfigureDapp {
+                dapp: "dapp".to_owned(),
+                percent: Some(89),
+                collector: Some("collector".to_string()),
+                repo_url: Some("repo.com".to_owned()),
+            },
+        })
+        .unwrap(),
+        expect![[
+            r#"{"referral_code":null,"configure_dapp":{"dapp":"dapp","percent":89,"collector":"collector","repo_url":"repo.com"}}"#
+        ]],
+    );
+
+    let msg: WithReferralCode<ExecuteMsg> = from_str(
+        r#"{
+            "referral_code": null,
+            "configure_dapp": {
+                "dapp": "dapp",
+                "percent": 89,
+                "collector": "collector",
+                "repo_url": "repo.com"
+            }
+        }"#,
+    )
+    .unwrap();
+
+    assert!(matches!(
+        msg,
+        WithReferralCode {
+            referral_code: None,
+            msg: ExecuteMsg::ConfigureDapp {
+                percent: Some(89),
+                ..
+            },
+        }
+    ));
+
+    check(
+        to_string(&WithReferralCode {
+            referral_code: None,
+            msg: ExecuteMsg::TransferOwnership {
+                code: 69,
+                owner: "owner".to_owned(),
+            },
+        })
+        .unwrap(),
+        expect![[r#"{"referral_code":null,"transfer_ownership":{"code":69,"owner":"owner"}}"#]],
+    );
+
+    let msg: WithReferralCode<ExecuteMsg> =
+        from_str(r#"{"transfer_ownership":{"code":69,"owner":"owner"}}"#).unwrap();
+
+    assert!(matches!(
+        msg,
+        WithReferralCode {
+            referral_code: None,
+            msg: ExecuteMsg::TransferOwnership { code: 69, .. },
+        }
+    ))
 }
